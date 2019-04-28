6af7586474d4aabb3ba00ed48e5b3af639cb0fed
Bartosz Przybysz <bartosz.d.przybysz@gmail.com>
2019-04-27 22:45:36 -0400

    Add get_tickers function to broker

--- broker/broker.py
+++ broker/broker.py
@@ -1,9 +1,13 @@
 import json
 from ib_insync import *
+import pandas as pd
+from typing import Set, Dict
 
-SETTINGS_PATH = 'settings\settings.json'
+SETTINGS_PATH = 'settings\\settings.json'
 TICKERS_PATH = 'settings\\tickers.xlsx'
 CACHE_DIR = 'cache'
+
+tickers = set()
 
 try:
     settings = json.load(open(SETTINGS_PATH, 'r'))
@@ -17,3 +21,20 @@ try:
 except Exception as e:
     print('Connecting to TWS failed')
     print(e)
+
+
+def get_tickers(path : str = TICKERS_PATH) -> Set[str]:
+    """
+    Get ticker symbols from an excel sheet. Return ticker symbols as a set
+    of strings
+
+    path -- path to excel sheet
+    """
+    sheet_data = pd.read_excel(path, skipna=True)
+
+    ticker_series = sheet_data.iloc[:, 0].dropna()
+
+    global tickers
+    tickers = set(ticker_series)
+
+    return tickers
